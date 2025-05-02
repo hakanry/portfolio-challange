65660edf86994c7f7b4b7aa16bb1ea2bf8519022
Hakan Reşit YALÇIN <hakanryalcin@gmail.com>
2025-05-02 16:48:51 +0300

    tailwind config updated

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,7 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      screens: {
-        xs: { max: "320px" },
-      },
+      screens: {},
     },
   },
   plugins: [],
